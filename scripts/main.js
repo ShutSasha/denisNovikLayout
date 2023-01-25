33701b7df4bfe2545454bf0bb78c532e9930d973
Sasha Shut <sanyashut159@gmail.com>
2023-01-25 21:16:34 +0200

    translate page -v0.1

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,14 @@
 const buttonLangUA = document.querySelector('.button-lang-UA');
 const buttonLangENG = document.querySelector('.button-lang-ENG');
-let select = 'en';
+let select;
 
 buttonLangUA.onclick = () => {
 	changeURLLanguage('ua');
-
-	setTimeout(() => {
-		buttonLangUA.style.color = '#070707';
-		buttonLangENG.style.color = '#828282';
-		buttonLangUA.style.transition = 'color .5s ease';
-		buttonLangENG.style.transition = 'color .4s ease';
-	}, 500);
-
 	select = 'ua';
 }
 
 buttonLangENG.onclick = () => {
-	setTimeout(() => {
-		changeURLLanguage('en')
-	}, 500);
-
-	buttonLangUA.style.color = '#828282';
-	buttonLangENG.style.color = '#070707';
-	buttonLangUA.style.transition = 'color .4s ease';
-	buttonLangENG.style.transition = 'color .5s ease';
+	changeURLLanguage('en')
 	select = 'en';
 
 }
@@ -34,27 +19,35 @@ const allLang = ['en', 'ua'];
 function changeURLLanguage (lang) {
 	location.href = window.location.pathname + '#' + lang;
 	location.reload();
-	console.log(location.href)
+	console.log(location.href);
 }
 
 function changeLanguage () {
 	let hash = window.location.hash;
-	hash = hash.substr(1);
-	console.log(hash);
+	hash = hash.slice(1);
+
 	if (!allLang.includes(hash)) {
 		location.href = window.location.pathname + '#en';
 		location.reload();
 	}
 	select = hash;
-	document.querySelector('title').innerHTML = langArr['unit'][hash];
+
+	if (String(select) == 'ua') {
+		buttonLangUA.classList.add('active-btn-ua');
+		buttonLangENG.classList.add('no-active-btn-eng');
+		buttonLangENG.classList.remove('active-btn-eng');
+	}
+	else {
+		buttonLangUA.classList.remove('active-btn-ua');
+		buttonLangENG.classList.add('active-btn-eng');
+	}
 
 	for (let key in langArr) {
 		let elem = document.querySelector('.lng-' + key);
 		if (elem) {
 			elem.innerHTML = langArr[key][hash];
 		}
-
 	}
-}
 
+}
 changeLanguage();
