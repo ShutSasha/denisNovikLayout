85de2d9975091c0f36cb7be07882342079a471ad
Sasha Shut <sanyashut159@gmail.com>
2023-01-25 23:56:51 +0200

    translate page BETA -v0.3

--- scripts/langs.js
+++ scripts/langs.js
@@ -11,7 +11,15 @@ const langArr = {
 		"en": "About me",
 		"ua": "Про мене",
 	},
+	".about-me-lng-1": {
+		"en": "About me",
+		"ua": "Про мене",
+	},
 	".skills-lng": {
+		"en": "Skills",
+		"ua": "Навички",
+	},
+	".skills-1-lng": {
 		"en": "Skills",
 		"ua": "Навички",
 	},
@@ -19,9 +27,64 @@ const langArr = {
 		"en": "Portfolio",
 		"ua": "Портфоліо",
 	},
+	".portfolio-1-lng": {
+		"en": "Portfolio",
+		"ua": "Портфоліо",
+	},
 	".contacts-lng": {
 		"en": "Contacts",
 		"ua": "Контакти",
 	},
-
+	".denis-novik": {
+		"en": "Denis Novik",
+		"ua": "Денис Новік",
+	},
+	".denis-novik-bio": {
+		"en": "UX | UI designer 24 years old, Minsk",
+		"ua": "UX | UI дизайнер, 24 роки, Мінськ",
+	},
+	".about-me-text-1-lng": {
+		"en": "Hi, I'm Denis – UX/UI designer from Minsk. I'm interested in design and everything connected with it.",
+		"ua": "Привіт, я Денис - UX/UI дизайнер з Мінська. Я зацікавлений дизайном і поєдную з цим все.",
+	},
+	".about-me-text-2-lng": {
+		"en": `I'm studying at courses "Web and mobile design interfaces" in IT-Academy.`,
+		"ua": `Я навчаюсь на курсах "Дизайн веб-сторінок та мобільних інтерфейсів" в ІТ-Академії`,
+	},
+	".about-me-text-3-lng": {
+		"en": `Ready to implement excellent projects with wonderful people.`,
+		"ua": `Готовий реалізовувати прекрасні проєкти з чудовими людьми`,
+	},
+	".skills-text-lng": {
+		"en": `I work in such programs as`,
+		"ua": `Я працюю в таких програмах як`,
+	},
+	".portfolio-link-1": {
+		"en": `Online fashion store - Homepage`,
+		"ua": `Інтернет-магазин модного одягу - Головна сторінка`,
+	},
+	".portfolio-link-2": {
+		"en": `Reebok Store - Concept`,
+		"ua": `Магазин Reedbok - Концепція`,
+	},
+	".portfolio-link-3": {
+		"en": `Braun Landing Page - Concept`,
+		"ua": `Сторінка Braun - Концепція`,
+	},
+	".contacts-lng-1": {
+		"en": `Contacts`,
+		"ua": `Контакти`,
+	},
+	".contacts-text-lng": {
+		"en": `Want to know more or just chat?<br>You are welcome!`,
+		"ua": `Хочеш знати більше чи просто написати повідомлення? Будь-ласка!`,
+	},
+	".contacts-btn-lng": {
+		"en": `Send message`,
+		"ua": `Надіслати повідомлення`,
+	},
+	".footer-text-lng": {
+		"en": `Like me on<br>LinkedIn, Instagram, Behance, Dribble`,
+		"ua": `Я в LinkedIn, Instagram, Behance, Dribble`,
+	},
 }
